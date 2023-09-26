9e65bda9e831ec211d195b96c43da6a098a9e587
Novák Dominik Viktor <ndominikviktor@mitiszkad.lan>
2023-09-26 09:04:19 +0200

    mukodo datagrid es add gomb

--- Ikt/Ikt/Adatok.cs
+++ Ikt/Ikt/Adatok.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Ikt
 {
-    internal class Adatok
+    public class Adatok
     {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
     }
 }
--- Ikt/Ikt/MainWindow.xaml.cs
+++ Ikt/Ikt/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,103 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Ikt
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
+        public partial class MainWindow : Window
         {
+            private MySqlConnection connection;
+            private MySqlCommand cmd;
+            private MySqlDataAdapter adapter;
+            private DataTable dataTable;
+
+            public MainWindow()
+            {
             InitializeComponent();
+
+            // Kapcsolódás az adatbázishoz
+            string connectionString = "Server=192.168.50.98;Database=ikt_sq;User=root;Password=password;";
+            connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                cmd = new MySqlCommand("SELECT * FROM contacts", connection);
+                adapter = new MySqlDataAdapter(cmd);
+                dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                dgContacts.ItemsSource = dataTable.DefaultView;
+
+                // Eseménykezelő hozzáadása a DataGrid-hez a kattintásra
+                dgContacts.MouseLeftButtonUp += DgContacts_MouseLeftButtonUp;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Hiba a kapcsolódás során: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        } 
+
+        private void DgContacts_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (dgContacts.SelectedItem is DataRowView selectedRow)
+            {
+               
+                txtFirstName.Text = selectedRow["first_name"].ToString();
+                txtLastName.Text = selectedRow["last_name"].ToString();
+                txtEmail.Text = selectedRow["email"].ToString();
+                txtPhone.Text = selectedRow["phone"].ToString();
+            }
         }
-    }
-}
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+            {
+                string insertQuery = "INSERT INTO contacts (first_name, last_name, email, phone) " +
+                                     "VALUES (@FirstName, @LastName, @Email, @Phone)";
+                MySqlCommand insertCmd = new MySqlCommand(insertQuery, connection);
+
+                insertCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                insertCmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                insertCmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                insertCmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+
+                try
+                {
+                    connection.Open();
+                    insertCmd.ExecuteNonQuery();
+                    dataTable.Clear();
+                    adapter.Fill(dataTable);
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Hiba az adatok hozzáadásakor: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+              
+
+
+        }
+
+            private void btnUpdate_Click(object sender, RoutedEventArgs e)
+            {
+                
+            }
+
+            private void btnDelete_Click(object sender, RoutedEventArgs e)
+            {
+                
+            }
+                
+        }
+
+ }
