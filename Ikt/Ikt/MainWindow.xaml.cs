3c390c514033a5218317f087b4e26ca47d4d3580
Novák Dominik Viktor <ndominikviktor@mitiszkad.lan>
2023-09-26 11:09:19 +0200

    mukodo delete gomb

--- Ikt/Ikt/MainWindow.xaml.cs
+++ Ikt/Ikt/MainWindow.xaml.cs
@@ -20,19 +20,19 @@ namespace Ikt
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        private MySqlConnection connection;
-        private MySqlCommand cmd;
-        private MySqlDataAdapter adapter;
-        private DataTable dataTable;
-
-        public MainWindow()
+        public partial class MainWindow : Window
         {
+            private MySqlConnection connection;
+            private MySqlCommand cmd;
+            private MySqlDataAdapter adapter;
+            private DataTable dataTable;
+
+            public MainWindow()
+            {
             InitializeComponent();
 
-            // Kapcsolódás az adatbázishoz
-            string connectionString = "Server=192.168.50.98;Database=ikt_sq;User=root;Password=password;";
+  
+            string connectionString = "Server=localhost;Database=ikt_sq;User=root;Password=;";
             connection = new MySqlConnection(connectionString);
 
             try
@@ -44,7 +44,7 @@ namespace Ikt
                 adapter.Fill(dataTable);
                 dgContacts.ItemsSource = dataTable.DefaultView;
 
-                // Eseménykezelő hozzáadása a DataGrid-hez a kattintásra
+                
                 dgContacts.MouseLeftButtonUp += DgContacts_MouseLeftButtonUp;
             }
             catch (MySqlException ex)
@@ -55,13 +55,13 @@ namespace Ikt
             {
                 connection.Close();
             }
-        }
+        } 
 
         private void DgContacts_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             if (dgContacts.SelectedItem is DataRowView selectedRow)
             {
-                // A kiválasztott sor adatainak beállítása a TextBox-okban
+      
                 txtFirstName.Text = selectedRow["first_name"].ToString();
                 txtLastName.Text = selectedRow["last_name"].ToString();
                 txtEmail.Text = selectedRow["email"].ToString();
@@ -70,74 +70,95 @@ namespace Ikt
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
-        {
-            string insertQuery = "INSERT INTO contacts (first_name, last_name, email, phone) " +
-                                 "VALUES (@FirstName, @LastName, @Email, @Phone)";
-            MySqlCommand insertCmd = new MySqlCommand(insertQuery, connection);
-
-            insertCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
-            insertCmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
-            insertCmd.Parameters.AddWithValue("@Email", txtEmail.Text);
-            insertCmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
-
-            try
             {
-                connection.Open();
-                insertCmd.ExecuteNonQuery();
-                dataTable.Clear();
-                adapter.Fill(dataTable);
-            }
-            catch (MySqlException ex)
-            {
-                MessageBox.Show("Hiba az adatok hozzáadásakor: " + ex.Message);
-            }
-            finally
-            {
-                connection.Close();
-            }
+                string insertQuery = "INSERT INTO contacts (first_name, last_name, email, phone) " +
+                                     "VALUES (@FirstName, @LastName, @Email, @Phone)";
+                MySqlCommand insertCmd = new MySqlCommand(insertQuery, connection);
 
-
-
-        }
-
-        private void btnUpdate_Click(object sender, RoutedEventArgs e)
-        {
-            if (dgContacts.SelectedItem is DataRowView selectedRow)
-            {
-                string updateQuery = "UPDATE contacts SET first_name = @FirstName, last_name = @LastName, " +
-                                     "email = @Email, phone = @Phone WHERE id = @Id";
-                MySqlCommand updateCmd = new MySqlCommand(updateQuery, connection);
-
-                updateCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
-                updateCmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
-                updateCmd.Parameters.AddWithValue("@Email", txtEmail.Text);
-                updateCmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
-                updateCmd.Parameters.AddWithValue("@Id", selectedRow["id"]);
+                insertCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                insertCmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                insertCmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                insertCmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
 
                 try
                 {
                     connection.Open();
-                    updateCmd.ExecuteNonQuery();
+                    insertCmd.ExecuteNonQuery();
                     dataTable.Clear();
                     adapter.Fill(dataTable);
                 }
                 catch (MySqlException ex)
                 {
-                    MessageBox.Show("Hiba az adatok frissítésekor: " + ex.Message);
+                    MessageBox.Show("Hiba az adatok hozzáadásakor: " + ex.Message);
                 }
                 finally
                 {
                     connection.Close();
                 }
+              
 
+
+        }
+
+            private void btnUpdate_Click(object sender, RoutedEventArgs e)
+            {
+                if (dgContacts.SelectedItem is DataRowView selectedRow)
+                {
+                    string updateQuery = "UPDATE contacts SET first_name = @FirstName, last_name = @LastName, " +
+                                         "email = @Email, phone = @Phone WHERE id = @Id";
+                    MySqlCommand updateCmd = new MySqlCommand(updateQuery, connection);
+
+                    updateCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                    updateCmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                    updateCmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    updateCmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+                    updateCmd.Parameters.AddWithValue("@Id", selectedRow["id"]);
+
+                    try
+                    {
+                        connection.Open();
+                        updateCmd.ExecuteNonQuery();
+                        dataTable.Clear();
+                        adapter.Fill(dataTable);
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show("Hiba az adatok frissítésekor: " + ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                   
+                }
             }
+
+            private void btnDelete_Click(object sender, RoutedEventArgs e)
+            {
+                if (dgContacts.SelectedItem is DataRowView selectedRow)
+                {
+                    string deleteQuery = "DELETE FROM contacts WHERE id = @Id";
+                    MySqlCommand deleteCmd = new MySqlCommand(deleteQuery, connection);
+                    deleteCmd.Parameters.AddWithValue("@Id", selectedRow["id"]);
+
+                    try
+                    {
+                        connection.Open();
+                        deleteCmd.ExecuteNonQuery();
+                        dataTable.Clear();
+                        adapter.Fill(dataTable);
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show("Hiba az adatok törlésekor: " + ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+                
+            }
+
         }
-
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
     }
-
-}
