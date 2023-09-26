5d58acf5c82b79d08fab9104f93224a22b4b4ca5
Novák Dominik Viktor <ndominikviktor@mitiszkad.lan>
2023-09-26 10:34:17 +0200

    mukodo update gomb

--- Ikt/Ikt/MainWindow.xaml.cs
+++ Ikt/Ikt/MainWindow.xaml.cs
@@ -20,15 +20,15 @@ namespace Ikt
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-        public partial class MainWindow : Window
-        {
-            private MySqlConnection connection;
-            private MySqlCommand cmd;
-            private MySqlDataAdapter adapter;
-            private DataTable dataTable;
+    public partial class MainWindow : Window
+    {
+        private MySqlConnection connection;
+        private MySqlCommand cmd;
+        private MySqlDataAdapter adapter;
+        private DataTable dataTable;
 
-            public MainWindow()
-            {
+        public MainWindow()
+        {
             InitializeComponent();
 
             // Kapcsolódás az adatbázishoz
@@ -55,13 +55,13 @@ namespace Ikt
             {
                 connection.Close();
             }
-        } 
+        }
 
         private void DgContacts_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             if (dgContacts.SelectedItem is DataRowView selectedRow)
             {
-               
+                // A kiválasztott sor adatainak beállítása a TextBox-okban
                 txtFirstName.Text = selectedRow["first_name"].ToString();
                 txtLastName.Text = selectedRow["last_name"].ToString();
                 txtEmail.Text = selectedRow["email"].ToString();
@@ -70,46 +70,74 @@ namespace Ikt
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
-            {
-                string insertQuery = "INSERT INTO contacts (first_name, last_name, email, phone) " +
-                                     "VALUES (@FirstName, @LastName, @Email, @Phone)";
-                MySqlCommand insertCmd = new MySqlCommand(insertQuery, connection);
+        {
+            string insertQuery = "INSERT INTO contacts (first_name, last_name, email, phone) " +
+                                 "VALUES (@FirstName, @LastName, @Email, @Phone)";
+            MySqlCommand insertCmd = new MySqlCommand(insertQuery, connection);
 
-                insertCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
-                insertCmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
-                insertCmd.Parameters.AddWithValue("@Email", txtEmail.Text);
-                insertCmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+            insertCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+            insertCmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+            insertCmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+            insertCmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+
+            try
+            {
+                connection.Open();
+                insertCmd.ExecuteNonQuery();
+                dataTable.Clear();
+                adapter.Fill(dataTable);
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Hiba az adatok hozzáadásakor: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgContacts.SelectedItem is DataRowView selectedRow)
+            {
+                string updateQuery = "UPDATE contacts SET first_name = @FirstName, last_name = @LastName, " +
+                                     "email = @Email, phone = @Phone WHERE id = @Id";
+                MySqlCommand updateCmd = new MySqlCommand(updateQuery, connection);
+
+                updateCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text);
+                updateCmd.Parameters.AddWithValue("@LastName", txtLastName.Text);
+                updateCmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                updateCmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+                updateCmd.Parameters.AddWithValue("@Id", selectedRow["id"]);
 
                 try
                 {
                     connection.Open();
-                    insertCmd.ExecuteNonQuery();
+                    updateCmd.ExecuteNonQuery();
                     dataTable.Clear();
                     adapter.Fill(dataTable);
                 }
                 catch (MySqlException ex)
                 {
-                    MessageBox.Show("Hiba az adatok hozzáadásakor: " + ex.Message);
+                    MessageBox.Show("Hiba az adatok frissítésekor: " + ex.Message);
                 }
                 finally
                 {
                     connection.Close();
                 }
-              
 
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
 
         }
 
-            private void btnUpdate_Click(object sender, RoutedEventArgs e)
-            {
-                
-            }
+    }
 
-            private void btnDelete_Click(object sender, RoutedEventArgs e)
-            {
-                
-            }
-                
-        }
-
- }
+}
